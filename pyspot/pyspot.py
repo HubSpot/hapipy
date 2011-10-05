d51fb2b1aac0ec724ee6dacc3c2a0f90b5f0e3f2
prior <prior@cracklabs.com>
2011-10-05 11:55:07 -0400

    add (very un-dry) timeout option to all methods and set 10s as default

--- pyspot/pyspot.py
+++ pyspot/pyspot.py
@@ -93,7 +93,7 @@ class HubSpotClient(object):
             return "atom+xml"
         return "json"
   
-    def _make_request(self, method, params, content_type, data=None, request_method='GET', url=None):
+    def _make_request(self, method, params, content_type, data=None, request_method='GET', url=None, timeout=10):
         params['hapikey'] = self.api_key
         
         if getattr(self,'hub_id'):
@@ -105,7 +105,7 @@ class HubSpotClient(object):
         if getattr(self,'environment','production').lower() == 'qa':
             domain = HUBSPOTQA_API_BASE
         
-        client = httplib.HTTPSConnection(domain)
+        client = httplib.HTTPSConnection(domain, timeout=timeout)
         if data and not isinstance(data, str):
             if request_method != 'PUT':  #and method doesn't contain 'blog'...this will hose update lead !!!!
                 data = urllib.urlencode(data)
@@ -139,39 +139,39 @@ class HubSpotLeadsClient(HubSpotClient):
     def create_lead(self, ip_address, cookie, fields):
         pass
     
-    def get_lead(self, lead_guid):
-        response = self._make_request('list/', {'guids[0]': lead_guid}, 'application/json')
+    def get_lead(self, lead_guid, timeout=10):
+        response = self._make_request('list/', {'guids[0]': lead_guid}, 'application/json', timeout=timeout)
         indv_lead = Lead(response['body'][0])
         return indv_lead
     
-    def search_leads(self, search_value):
-        response = self._make_request('list/', {'search': search_value}, 'application/json')
+    def search_leads(self, search_value, timeout=10):
+        response = self._make_request('list/', {'search': search_value}, 'application/json', timeout=timeout)
         lead_objs = []
         for indv_leads in response['body']:
             indv_lead_obj = Lead(indv_leads)
             lead_objs.append(indv_lead_obj)
         return lead_objs
   
-    def update_lead(self, lead_guid, update_data={}):
-        response = self._make_request('lead/%s/' % lead_guid, {}, 'application/json', str(update_data), request_method='PUT')
+    def update_lead(self, lead_guid, update_data={}, timeout=10):
+        response = self._make_request('lead/%s/' % lead_guid, {}, 'application/json', str(update_data), request_method='PUT', timeout=timeout)
         if response['status'] == 200:
             lead_response = self._make_request('list/', {'guids[0]': lead_guid}, 'application/json')
             indv_lead = Lead(lead_response['body'][0])
             return indv_lead
   
-    def get_webhook(self):  #WTF are these 2 methods for?
-        return self._make_request('callback-url', {}, 'application/json')
+    def get_webhook(self, timeout=10):  #WTF are these 2 methods for?
+        return self._make_request('callback-url', {}, 'application/json', timeout=10)
     
-    def register_webhook(self, url):
-        return self._make_request('callback-url', {'url': url}, 'application/json', data={'url': url}, request_method='POST')
+    def register_webhook(self, url, timeout=10):
+        return self._make_request('callback-url', {'url': url}, 'application/json', data={'url': url}, request_method='POST', timeout=timeout)
     
-    def close_lead(self, lead_guid, close_time=None):
+    def close_lead(self, lead_guid, close_time=None, timeout=10):
         if close_time == None:
             now = int(time.time()*1000)
             data = '{\'guid\':\'%s\', \'closedAt\': \'%s\'}' % (lead_guid, now)
         else:
             data = '{\'guid\':\'%s\', \'closedAt\': \'%s\'}' % (lead_guid, close_time)
-        response = self._make_request('lead/%s/' % lead_guid, {}, 'application/json', str(data), request_method='PUT')
+        response = self._make_request('lead/%s/' % lead_guid, {}, 'application/json', str(data), request_method='PUT',timeout=timeout)
         if response['status'] == 200:
             lead_response = self._make_request('list/', {'guids[0]': lead_guid}, 'application/json')
             indv_lead = Lead(lead_response['body'][0])
@@ -183,40 +183,40 @@ class HubSpotLeadNurtureClient(HubSpotClient):
     def _create_path(self, method):
         return 'nurture/v%s/%s' % (HUBSPOT_LEADS_API_VERSION, method)
     
-    def get_campaigns(self):
-        response = self._make_request('campaigns', {}, 'application/json')
+    def get_campaigns(self, timeout=10):
+        response = self._make_request('campaigns', {}, 'application/json',timeout=timeout)
         ln_campaigns = []
         for lnc in response['body']:
             lnc_obj = LeadNurturingCampaign(lnc)
             ln_campaigns.append(lnc_obj)
         return ln_campaigns
   
-    def get_leads(self, campaign_guid):
-        response = self._make_request('campaign/%s/list' % campaign_guid, {}, 'application/json')
+    def get_leads(self, campaign_guid, timeout=10):
+        response = self._make_request('campaign/%s/list' % campaign_guid, {}, 'application/json', timeout=timeout)
         leads_in_campaign = []
         for leads in response['body']:
             leads_in_ln = CampaignLeads(leads)
             leads_in_campaign.append(leads_in_ln)
         return leads_in_campaign
     
-    def get_history(self, lead_guid):
-        response = self._make_request('lead/%s' % lead_guid, {}, 'application/json')
+    def get_history(self, lead_guid, timeout=10):
+        response = self._make_request('lead/%s' % lead_guid, {}, 'application/json', timeout=timeout)
         leads_in_campaigns = []
         for leads in response['body']:
             leads_in_ln = CampaignLeads(leads)
             leads_in_campaigns.append(leads_in_ln)
         return leads_in_campaigns
   
-    def enroll_lead(self, campaign_guid, lead_guid):
-        response = self._make_request('campaign/%s/add' % campaign_guid, {}, 'application/json', str(lead_guid), request_method='POST')
+    def enroll_lead(self, campaign_guid, lead_guid, timeout=10):
+        response = self._make_request('campaign/%s/add' % campaign_guid, {}, 'application/json', str(lead_guid), request_method='POST', timeout=timeout)
         if response['status'] == 200:
             message = "lead enrolled"
         else:
             message = "failed to enroll lead"
         return message
   
-    def unenroll_lead(self, campaign_guid, lead_guid):
-        response = self._make_request('campaign/%s/remove' % campaign_guid, {}, 'application/json', str(lead_guid), request_method='POST')
+    def unenroll_lead(self, campaign_guid, lead_guid, timeout=10):
+        response = self._make_request('campaign/%s/remove' % campaign_guid, {}, 'application/json', str(lead_guid), request_method='POST',timeout=timeout)
         if response['status'] == 200:
             message = "lead unenrolled"
         else:
@@ -239,12 +239,12 @@ class HubSpotEventClient(HubSpotClient):
         }
         return messages[code]
     
-    def get_events(self):
+    def get_events(self, timeout=10):
         return self._make_request(
-            'events', {}
+            'events', {}, timeout=timeout
         )
     
-    def create_event(self, description, create_date, url, event_type):
+    def create_event(self, description, create_date, url, event_type, timeout=10):
         event_data = {
             'description': description,
             'createDate': create_date,
@@ -252,7 +252,7 @@ class HubSpotEventClient(HubSpotClient):
             'eventType': event_type
         }
         return self._make_request(
-            'events', {}, data=event_data, request_method='POST'
+            'events', {}, data=event_data, request_method='POST', timeout=timeout
         )
 
 
@@ -271,11 +271,11 @@ class HubSpotSettingsClient(HubSpotClient):
         }
         return messages[code]
   
-    def get_settings(self):
-        return self._make_request('settings', {}, 'application/json')    
+    def get_settings(self, timeout=10):
+        return self._make_request('settings', {}, 'application/json', timeout=timeout)    
     
-    def update_settings(self, data):
-        return self._make_request('settings', {}, 'application/json', data=data, request_method='POST')
+    def update_settings(self, data, timeout=10):
+        return self._make_request('settings', {}, 'application/json', data=data, request_method='POST', timeout=timeout)
   
 
 class HubSpotBlogClient(HubSpotClient):
@@ -293,70 +293,70 @@ class HubSpotBlogClient(HubSpotClient):
         }
         return messages[code]
   
-    def get_blogs(self):
-        hs_response = self._make_request('list.json', {}, 'application/json')
+    def get_blogs(self, timeout=10):
+        hs_response = self._make_request('list.json', {}, 'application/json', timeout=timeout)
         blog_objs = []
         for blog_json in hs_response['body']:
             individual_blog_obj = Blog(blog_json)
             blog_objs.append(individual_blog_obj)
         return blog_objs
   
-    def get_blog_info(self, blog_guid):
-        hs_response = self._make_request(blog_guid, {}, 'application/json')
+    def get_blog_info(self, blog_guid, timeout=10):
+        hs_response = self._make_request(blog_guid, {}, 'application/json', timeout=timeout)
         individual_blog_obj = Blog(hs_response['body'])
         return individual_blog_obj
   
-    def get_posts(self, blog_guid):
-        hs_response = self._make_request('%s/posts.json' % blog_guid, {}, 'application/json')
+    def get_posts(self, blog_guid, timeout=10):
+        hs_response = self._make_request('%s/posts.json' % blog_guid, {}, 'application/json', timeout=timeout)
         blog_post_objs = []
         for blog_posts in hs_response['body']:
             individual_blog_obj = BlogPosts(blog_posts)
             blog_post_objs.append(individual_blog_obj)
         return blog_post_objs
   
-    def get_drafts(self, blog_guid):
-        hs_response = self._make_request('%s/posts' % blog_guid, {'draft': 'true'}, 'application/json')
+    def get_drafts(self, blog_guid, timeout=10):
+        hs_response = self._make_request('%s/posts' % blog_guid, {'draft': 'true'}, 'application/json',timeout=timeout)
         blog_post_objs = []
         for blog_posts in hs_response['body']:
             individual_blog_obj = BlogPosts(blog_posts)
             blog_post_objs.append(individual_blog_obj)
         return blog_post_objs
   
-    def get_published_posts(self, blog_guid):
-        hs_response = self._make_request('%s/posts' % blog_guid, {'draft': 'false'}, 'application/json')
+    def get_published_posts(self, blog_guid, timeout=10):
+        hs_response = self._make_request('%s/posts' % blog_guid, {'draft': 'false'}, 'application/json', timeout=timeout)
         blog_post_objs = []
         for blog_posts in hs_response['body']:
             individual_blog_obj = BlogPosts(blog_posts)
             blog_post_objs.append(individual_blog_obj)
         return blog_post_objs
   
-    def get_blog_comments(self, blog_guid):
-        hs_response = self._make_request('%s/comments.json' % blog_guid, {}, 'application/json')
+    def get_blog_comments(self, blog_guid, timeout=10):
+        hs_response = self._make_request('%s/comments.json' % blog_guid, {}, 'application/json', timeout=timeout)
         blog_comment_objs = []
         for blog_comments in hs_response['body']:
             individual_comment_obj = BlogComment(blog_comments)
             blog_comment_objs.append(individual_comment_obj)
         return blog_comment_objs
   
-    def get_post(self, post_guid):
-        hs_response = self._make_request('posts/%s.json' % post_guid, {}, 'application/json')
+    def get_post(self, post_guid, timeout=10):
+        hs_response = self._make_request('posts/%s.json' % post_guid, {}, 'application/json', timeout=timeout)
         individual_post_obj = BlogPosts(hs_response['body'])
         return individual_post_obj
     
-    def get_post_comments(self, post_guid):
-        hs_response = self._make_request('posts/%s/comments.json' % post_guid, {}, 'application/json')
+    def get_post_comments(self, post_guid, timeout=10):
+        hs_response = self._make_request('posts/%s/comments.json' % post_guid, {}, 'application/json', timeout=timeout)
         blog_comment_objs = []
         for blog_comments in hs_response['body']:
             individual_comment_obj = BlogComment(blog_comments)
             blog_comment_objs.append(individual_comment_obj)
         return blog_comment_objs
       
-    def get_comment(self, comment_guid):
-        hs_response = self._make_request('comments/%s.json' % comment_guid, {}, 'application/json')
+    def get_comment(self, comment_guid, timeout=10):
+        hs_response = self._make_request('comments/%s.json' % comment_guid, {}, 'application/json', timeout=timeout)
         individual_comment_obj = BlogComment(hs_response['body'])
         return individual_comment_obj
     
-    def create_post(self, blog_guid, author_name, author_email, title, summary, content, tags):
+    def create_post(self, blog_guid, author_name, author_email, title, summary, content, tags, timeout=10):
         tag_xml = ''
         for tag in tags:
             tag_xml += '<category term="tag %s" />' % tag
@@ -371,12 +371,12 @@ class HubSpotBlogClient(HubSpotClient):
                     <content type="html"><![CDATA[%s]]></content>
                     %s
                 </entry>''' % (title, author_name, author_email, summary, content, tag_xml)
-        hs_response = self._make_request('%s/posts.atom' % blog_guid, {}, content_type='application/atom+xml', data=post, request_method='POST')
+        hs_response = self._make_request('%s/posts.atom' % blog_guid, {}, content_type='application/atom+xml', data=post, request_method='POST', timeout=timeout)
         parsed_xml = minidom.parseString(hs_response['body'])
         inv_blog_post_obj = BlogPostCreate(parsed_xml)
         return inv_blog_post_obj
   
-    def update_post(self, post_guid, title, summary, content, meta_desc, meta_keyword, tags):
+    def update_post(self, post_guid, title, summary, content, meta_desc, meta_keyword, tags, timeout=10):
         tag_xml = ''
         for tag in tags:
             tag_xml += '<category term="tag %s" />' % tag
@@ -389,24 +389,24 @@ class HubSpotBlogClient(HubSpotClient):
                     <hs:metaDescription>%s</hs:metaDescription>
                     <hs:metaKeywords>%s</hs:metaKeywords>
                 </entry>''' % (title, summary, content, tag_xml, meta_desc, meta_keyword)
-        hs_response = self._make_request('posts/%s.atom' % post_guid, {}, content_type='application/atom+xml', data=post, request_method='PUT')
+        hs_response = self._make_request('posts/%s.atom' % post_guid, {}, content_type='application/atom+xml', data=post, request_method='PUT', timeout=timeout)
         parsed_xml = minidom.parseString(hs_response['body'])
         inv_blog_post_obj = BlogPostCreate(parsed_xml)
         return inv_blog_post_obj
   
-    def publish_post(self, post_guid, publish_time, is_draft, should_notify):
+    def publish_post(self, post_guid, publish_time, is_draft, should_notify, timeout=10):
         post = '''<?xml version="1.0" encoding="utf-8"?>
                 <entry xmlns="http://www.w3.org/2005/Atom" xmlns:hs="http://www.hubspot.com/">
                     <published>%s</published>
                     <hs:draft>%s</hs:draft>
                     <hs:sendNotifications>%s</hs:sendNotifications>
                 </entry>''' % (publish_time, is_draft, should_notify)
-        hs_response = self._make_request('posts/%s.atom' % post_guid, {}, content_type = 'application/atom+xml', data=post, request_method='PUT')
+        hs_response = self._make_request('posts/%s.atom' % post_guid, {}, content_type = 'application/atom+xml', data=post, request_method='PUT', timeout=timeout)
         parsed_xml = minidom.parseString(hs_response['body'])
         inv_blog_post_obj = BlogPostCreate(parsed_xml)
         return inv_blog_post_obj
     
-    def create_comment(self, post_guid, author_name, author_email, author_uri, content):
+    def create_comment(self, post_guid, author_name, author_email, author_uri, content, timeout=10):
         post = '''<?xml version="1.0" encoding="utf-8"?>
                 <entry xmlns="http://www.w3.org/2005/Atom">
                     <author>
@@ -416,7 +416,7 @@ class HubSpotBlogClient(HubSpotClient):
                     </author>
                     <content type="html"><![CDATA[%s]]></content>
                 </entry>''' % (author_name, author_email, author_uri, content)
-        hs_response = self._make_request('posts/%s/comments.atom' % post_guid, {}, content_type='application/atom+xml', data=post, request_method='POST')
+        hs_response = self._make_request('posts/%s/comments.atom' % post_guid, {}, content_type='application/atom+xml', data=post, request_method='POST', timeout=timeout)
         #print hs_response['body']
         parsed_xml = minidom.parseString(hs_response['body'])
         inv_blog_post_obj = BlogCommentCreate(parsed_xml)

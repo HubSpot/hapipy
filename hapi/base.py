5942991a8c64f8f13633dd6ff5b92b4e07b59577
Bryan Beaudreault <bbeaudreault@hubspot.com>
2011-10-22 16:37:15 -0400

    Added a mixins directory, with the firs mixin being PyCurlMixin for threaded api calls.  Modified base.py to make it more easily extensible (broke out helpers)

--- hapi/base.py
+++ hapi/base.py
@@ -24,46 +24,62 @@ class BaseClient(object):
         parts = self.options['api_base'].split('://')
         protocol = (parts[0:-1]+['https'])[0]
         self.options['connection_type'] = connection_types[protocol]
+        self.options['protocol'] = protocol
         self.options['api_base'] = parts[-1]
 
     def _get_path(self, subpath):
         raise Exception("Unimplemented get_path for BaseClient subclass!")
 
-    def _create_request(self, conn, subpath, params, method, data, opts):
+    def _prepare_request(self, subpath, params, data, opts):
         params = params or {}
         params['hapikey'] = self.api_key
         if opts.get('hub_id') or opts.get('portal_id'):
             params['portalId'] = opts.get('hub_id') or opts.get('portal_id')
         url = opts.get('url') or '/%s?%s' % (self._get_path(subpath), urllib.urlencode(params))
         headers = {'Content-Type': opts.get('content_type') or 'application/json'}
-        if not isinstance(data, str) and headers['Content-Type']=='application/json':
+        if data and not isinstance(data, str) and headers['Content-Type']=='application/json':
             data = json.dumps(data)
-        #logger.debug("%s %s%s  %s %s", (method, opts['api_base'], url, data, headers))
+
+        return url, headers, data
+
+    def _create_request(self, conn, method, url, headers, data):
         conn.request(method, url, data, headers)
         return {'method':method, 'url':url, 'data':data, 'headers':headers, 'host':conn.host, 'timeout':conn.timeout}
 
-    def _digest_result(self, conn, request):
+    def _execute_request(self, conn, request):
         result = conn.getresponse()
         result.body = result.read()
-        #logger.debug("%s %s\n---message---\n%s\n---body---\n%s\n---headers---\n%s\n",
-                #(result.status, result.reason, result.msg, result_read, result.getheaders()))
+        
         data = result.body
         conn.close()
         if result.status >= 400:
             raise HapiError(result, request)
+
+        return data
+
+    def _digest_result(self, data):
         if data and isinstance(data, str):
             try:
                 data = json.loads(data)
             except ValueError:  
                 pass
+
         return data
 
     def _call(self, subpath, params=None, method='GET', data=None, **options):
         opts = self.options.copy()
         opts.update(options)
 
+        url, headers, data = self._prepare_request(subpath, params, data, opts)
+
         connection = opts['connection_type'](opts['api_base'], timeout=opts['timeout'])
-        request_info = self._create_request(connection, subpath, params, method, data, opts)
-        return self._digest_result(connection, request_info)
+        request_info = self._create_request(connection, method, url, headers, data)
+
+        data = self._execute_request(connection, request_info)
+        return self._digest_result(data)
+
+    def mixin(self, mixin_class):
+        if mixin_class not in self.__class__.__bases__:
+            self.__class__.__bases__ = (mixin_class,) + self.__class__.__bases__
 
     
---
+++ hapi/mixins/threading.py
@@ -0,0 +1,135 @@
+import pycurl, cStringIO
+
+class HapiThreadedError(ValueError):
+    def __init__(self, curl):
+        super(HapiThreaded, self).__init__(c.body)
+        self.c = curl
+        self.response_body = self.c.body.getvalue()
+        self.response_headers = self.c.response_headers.getvalue()
+
+    def __str__(self):
+        return "\n---- request ----\n%s %s%s [timeout=%s]\n\n---- body ----\n%s\n\n---- headers ----\n%s\n\n---- result ----\n%s\n\n---- body ----\n%s\n\n---- headers ----\n%s" % (
+            self.c.getattr('method', ''), 
+            self.c.host, 
+            self.c.path, 
+            self.c.timeout,
+            self.c.data,
+            self.c.headers,
+            self.c.status,
+            self.response_body,
+            self.response_headers)
+
+    def __unicode__(self):
+        return self.__str__()
+
+class PyCurlMixin(object):
+    """
+    PyCurlMixin relies on PyCurl, which is a library around libcurl which enables efficient
+    multi-threaded requests.  Use this mixin when you want to be able to execute multiple
+    API calls at once, instead of in sequence.
+
+    Enable by calling client.mixin(PyCurlMixin) after importing PyCurlMixin and instantiating
+    the client of your choice.
+
+    Once enabled, use like so:
+        client.my_api_call()
+        client.my_other_api_call()
+        results = client.process_queue()
+
+    The results object will then return a list of dicts, containing the response to your calls
+    in the order they were called. Dicts have keys: result, code, and (if something went wrong) exception.
+    """
+    _queue = []
+
+    def _call(self, subpath, params=None, method='GET', data=None, **options):
+        opts = self.options.copy()
+        opts.update(options)
+
+        request_parts = self._prepare_request(subpath, params, data, opts)
+        self._enqueue(request_parts)
+
+    def _enqueue(self, parts):
+        self._queue.append(parts)
+
+    def _create_curl(self, url, headers, data):
+        c = pycurl.Curl()
+
+        full_url = "%s://%s%s" % (self.options['protocol'], self.options['api_base'], url)
+        
+        c.timeout = self.options['timeout']
+        c.protocol = self.options['protocol']
+        c.host = self.options['api_base']
+        c.path = url
+        c.full_url = full_url
+        c.headers = headers
+        c.data = data
+
+        c.status = -1
+        c.body = cStringIO.StringIO()
+        c.response_headers = cStringIO.StringIO()
+
+        c.setopt(c.URL, c.full_url)
+        c.setopt(c.TIMEOUT, self.options['timeout'])
+        c.setopt(c.WRITEFUNCTION, c.body.write)
+        c.setopt(c.HEADERFUNCTION, c.response_headers.write)
+
+        if headers:
+            c.setopt(c.HTTPHEADER, [ "%s: %s" % (x, y) for x, y in headers.items() ])
+
+        if data:
+            c.data_out = cStringIO.StringIO(data)
+            c.setopt(c.READFUNCTION, c.data_out.getvalue)
+
+        return c
+
+    def process_queue(self):
+        """
+        Processes all API calls since last invocation, returning a list of data
+        in the order the API calls were created
+        """
+        m = pycurl.CurlMulti()
+        m.handles = []
+
+        # Loop the queue and create Curl objects for processing
+        for item in self._queue:
+            c = self._create_curl(*item)
+            m.add_handle(c)
+            m.handles.append(c)
+
+        # Process the collected Curl handles
+        num_handles = len(m.handles)
+        while num_handles:
+            while 1:
+                # Perform the calls
+                ret, num_handles = m.perform()
+                if ret != pycurl.E_CALL_MULTI_PERFORM:
+                    break
+            m.select(1.0)
+
+        # Collect data
+        data = []
+        for c in m.handles:
+            c.status = c.getinfo(c.HTTP_CODE)
+            result = { "result" : self._digest_result(c.body.getvalue()), "code": c.status }
+            if c.status >= 400:
+                # Don't throw the exception because some might have succeeded
+                result['exception'] = HapiThreadedError(c)
+
+            data.append(result)
+
+            
+        # cleanup
+        for c in m.handles:
+            if hasattr(c, "data_out"):
+                c.data_out.close()
+
+            c.body.close()
+            c.response_headers.close()
+            c.close()
+            m.remove_handle(c)
+
+        m.close()
+        del m.handles
+        self._queue = []
+
+        return data

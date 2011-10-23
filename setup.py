8a4652eb595f7663bef437a88038465c34d5ee05
Bryan Beaudreault <bbeaudreault@hubspot.com>
2011-10-23 13:17:29 -0400

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='hapipy',
-    version='2.1.0',
+    version='2.1.1',
     description='Python HubSpot Api Wrapper',
     author='Adrian Mott',
     author_email='adrianmott@gmail.com',

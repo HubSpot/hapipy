ba462399ab44d6deb2584aa9bea50c28188cbaec
prior <prior@cracklabs.com>
2011-10-24 01:14:57 -0400

    build out the setup a bit more for the cheeseshop

--- setup.py
+++ setup.py
@@ -4,14 +4,18 @@ from distutils.core import setup
 setup(
     name='hapipy',
     version='2.1.4',
-    description='Python HubSpot Api Wrapper',
+    description="A python wrapper around HubSpot's APIs",
     author='Adrian Mott',
-    author_email='adrianmott@gmail.com',
-    url='',
+    author_email='amott@hubspot.com',
+    url='https://github.com/HubSpot/hapipy',
+    download_url='https://github.com/HubSpot/hapipy/tarball/v2.1.4',
     packages=['hapi'],
     install_requires=[
         'nose==1.1.2',
         'unittest2==0.5.1',
         'simplejson==2.2.1'
-    ]
+    ],
+    long_description = """
+        Full docs at: https://github.com/HubSpot/hapipy
+    """
 )

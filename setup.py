66a13bbd2f6f40d20d670c8e0ffd07867c5d99ce
prior <prior@cracklabs.com>
2011-10-24 10:38:34 -0400

    adjust to be more cheese friendly

--- setup.py
+++ setup.py
@@ -5,17 +5,16 @@ setup(
     name='hapipy',
     version='2.1.4',
     description="A python wrapper around HubSpot's APIs",
+    long_description = open('README.md').read(),
     author='Adrian Mott',
     author_email='amott@hubspot.com',
     url='https://github.com/HubSpot/hapipy',
     download_url='https://github.com/HubSpot/hapipy/tarball/v2.1.4',
+    license='LICENSE.txt',
     packages=['hapi'],
     install_requires=[
         'nose==1.1.2',
         'unittest2==0.5.1',
         'simplejson==2.2.1'
     ],
-    long_description = """
-        Full docs at: https://github.com/HubSpot/hapipy
-    """
 )

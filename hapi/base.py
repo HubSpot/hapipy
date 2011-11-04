ad582af086c3c5b047e7fd09e2ddbb0c0b398c45
prior <prior@cracklabs.com>
2011-11-03 22:02:36 -0400

    don't try to convert unicode strings

--- hapi/base.py
+++ hapi/base.py
@@ -44,7 +44,7 @@ class BaseClient(object):
             params['portalId'] = opts.get('hub_id') or opts.get('portal_id')
         url = opts.get('url') or '/%s?%s' % (self._get_path(subpath), urllib.urlencode(params))
         headers = {'Content-Type': opts.get('content_type') or 'application/json'}
-        if data and not isinstance(data, str) and headers['Content-Type']=='application/json':
+        if data and not isinstance(data, basestring) and headers['Content-Type']=='application/json':
             data = json.dumps(data)
 
         return url, headers, data

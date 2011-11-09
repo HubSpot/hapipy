1535153599067b4f1fad4f9b5b42fc157ef32b97
Jessica Scott <jscott@hubspot.com>
2011-11-09 18:19:55 -0500

    Adding Keyword tests, tweaking list results a bit so it turns an array instead of an object.

--- hapi/keywords.py
+++ hapi/keywords.py
@@ -8,7 +8,7 @@ class KeywordsClient(BaseClient):
         return 'keywords/v%s/%s' % (KEYWORDS_API_VERSION, subpath)
     
     def get_keywords(self, **options):
-        return self._call('keywords', **options)
+        return self._call('keywords', **options)['keywords']
     
     def get_keyword(self, keyword_guid, **options):
         return self._call('keywords/%s' % keyword_guid, **options)
@@ -21,7 +21,7 @@ class KeywordsClient(BaseClient):
         for keyword in keywords:
             if keyword != '':
                 data.append(dict(keyword=str(keyword)))
-        return self._call('keywords', data=data, method='PUT', **options)
+        return self._call('keywords', data=data, method='PUT', **options)['keywords']
     
     def delete_keyword(self, keyword_guid, **options):
         return self._call('keywords/%s' % keyword_guid, method='DELETE', **options)

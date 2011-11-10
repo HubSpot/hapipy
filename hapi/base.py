446d72bd57104229a2c8592d344c34193b74d612
Andy Aylward <aaylward@gmail.com>
2011-11-10 15:01:53 -0500

    rolling back changes for now. Later, I'll preserve the default timeout for Python > 2.5

--- hapi/base.py
+++ hapi/base.py
@@ -3,19 +3,20 @@ import httplib
 import simplejson as json
 from error import HapiError
 
-#class Request(object):
-    #def __init__(self, method, url, data, headers)
-
-        #super(BaseClient, self).__init__()
-
-
 class BaseClient(object):
     '''Base abstract object for interacting with the HubSpot APIs'''
 
-    def __init__(self, api_key, **extra_options):
+    def __init__(self, api_key, timeout=10, mixins=[], **extra_options):
         super(BaseClient, self).__init__()
+        # reverse so that the first one in the list because the first parent
+        mixins.reverse()
+        for mixin_class in mixins:
+            if mixin_class not in self.__class__.__bases__:
+                self.__class__.__bases__ = (mixin_class,) + self.__class__.__bases__
+
+
         self.api_key = api_key
-        self.options = {'api_base':'hubapi.com'}
+        self.options = {'timeout':timeout, 'api_base':'hubapi.com'}
         self.options.update(extra_options)
         self._prepare_connection_type()
 
@@ -44,7 +45,7 @@ class BaseClient(object):
 
     def _create_request(self, conn, method, url, headers, data):
         conn.request(method, url, data, headers)
-        return {'method':method, 'url':url, 'data':data, 'headers':headers, 'host':conn.host}
+        return {'method':method, 'url':url, 'data':data, 'headers':headers, 'host':conn.host, 'timeout':conn.timeout}
 
     def _execute_request(self, conn, request):
         result = conn.getresponse()
@@ -72,14 +73,11 @@ class BaseClient(object):
 
         url, headers, data = self._prepare_request(subpath, params, data, opts)
 
-        connection = opts['connection_type'](opts['api_base'],)
+        connection = opts['connection_type'](opts['api_base'], timeout=opts['timeout'])
         request_info = self._create_request(connection, method, url, headers, data)
 
         data = self._execute_request(connection, request_info)
         return self._digest_result(data)
 
-    def mixin(self, mixin_class):
-        if mixin_class not in self.__class__.__bases__:
-            self.__class__.__bases__ = (mixin_class,) + self.__class__.__bases__
-
+        
     

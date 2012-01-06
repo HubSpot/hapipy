093bb6f8ebb62b337ab476e4c5f833b3f9286f2d
Yoav Shapira <yoavs@yoavshapira.com>
2012-01-06 11:58:31 -0500

    Bumping version in setup.py for new LEads API method added.

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 
 setup(
     name='hapipy',
-    version='2.5.0',
+    version='2.5.1',
     description="A python wrapper around HubSpot's APIs",
     long_description = open('README.md').read(),
     author='HubSpot Dev Team',
     author_email='devteam+hapi@hubspot.com',
     url='https://github.com/HubSpot/hapipy',
-    download_url='https://github.com/HubSpot/hapipy/tarball/v2.5.0',
+    download_url='https://github.com/HubSpot/hapipy/tarball/v2.5.1',
     license='LICENSE.txt',
     packages=['hapi'],
     install_requires=[

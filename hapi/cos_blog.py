90f58bf1623a096f77268b6a29b2fcc4508410cf
Dan Craig <dan@hivefire.com>
2014-01-10 14:08:53 -0500

    COS Blog API - Blogs, Blog Posts DocStrings

--- hapi/cos_blog.py
+++ hapi/cos_blog.py
@@ -11,15 +11,19 @@ class COSBlogClient(BaseClient):
         return 'content/api/v%s/%s' % (BLOG_API_VERSION, subpath)
 
     def get_blogs(self, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blogs'''
         return self._call('blogs', **options)
 
     def get_blog(self, blog_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blogs_blog_id'''
         return self._call('blogs/%s' % blog_id, **options)
 
     def get_blog_versions(self, blog_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blogs_blog_id_versions'''
         return self._call('blogs/%s/versions' % blog_id, **options)
 
     def get_blog_version(self, blog_id, version_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blogs_blog_id_versions_version_id'''
         return self._call('blogs/%s/versions/%s' % (blog_id, version_id), **options)
 
     # Blog Posts
@@ -41,12 +45,13 @@ class COSBlogClient(BaseClient):
                     is_draft=None, meta_description=None, meta_keyworks=None,
                     post_body=None, post_summary=None, publish_date=None,
                     publish_immediately=None, slug=None, topic_ids=None, widgets=None, **options):
-
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts'''
         data = self._post_data(**locals())
         return self._call('blog-posts', data=json.dumps(data), method='POST',
                           content_type='application/json',**options)
 
     def get_posts(self, query={}, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts'''
         return self._call('blog-posts', query=urlencode(query), **options)
 
     def update_post(self, blog_post_id, content_group_id=None, name=None, blog_author_id=None,
@@ -54,14 +59,17 @@ class COSBlogClient(BaseClient):
                     is_draft=None, meta_description=None, meta_keyworks=None,
                     post_body=None, post_summary=None, publish_date=None,
                     publish_immediately=None, slug=None, topic_ids=None, widgets=None, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/put_blog_posts_blog_post_id'''
         data = self._post_data(**locals())
         return self._call('blog-posts/%s' % blog_post_id, data=json.dumps(data), method='PUT',
                           content_type='application/json',**options)
 
     def delete_post(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/delete_blog_posts_blog_post_id'''
         return self._call('blog-posts/%s' % blog_post_id, method='DELETE', **options)
 
     def get_post(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts_blog_post_id'''
         return self._call('blog-posts/%s' % blog_post_id, **options)
 
     def update_auto_save_buffer(self, blog_post_id, content_group_id=None, name=None, blog_author_id=None,
@@ -69,11 +77,13 @@ class COSBlogClient(BaseClient):
                                 is_draft=None, meta_description=None, meta_keyworks=None,
                                 post_body=None, post_summary=None, publish_date=None,
                                 publish_immediately=None, slug=None, topic_ids=None, widgets=None, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/put_blog_posts_blog_post_id_buffer'''
         data = self._post_data(**locals())
         return self._call('blog-posts/%s/buffer' % blog_post_id, data=json.dumps(data), method='PUT',
                           content_type='application/json',**options)
 
     def get_auto_save_buffer(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts_blog_post_id_buffer'''
         return self._call('blog-posts/%s/buffer' % blog_post_id, **options)
 
     def clone_post(self, blog_post_id, content_group_id=None, name=None, blog_author_id=None,
@@ -81,35 +91,44 @@ class COSBlogClient(BaseClient):
                    is_draft=None, meta_description=None, meta_keyworks=None,
                    post_body=None, post_summary=None, publish_date=None,
                    publish_immediately=None, slug=None, topic_ids=None, widgets=None, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_clone'''
         data = self._post_data(**locals())
         return self._call('blog-posts/%s/clone' % blog_post_id, data=json.dumps(data), method='POST',
                           content_type='application/json',**options)
 
     def get_buffered_changes(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts_blog_post_id_has_buffered_changes'''
         return self._call('blog-posts/%s/has-buffered-changes' % blog_post_id, **options)
 
     def publish_post(self, blog_post_id, action, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_publish_action'''
         if action not in ('push-buffer-live', 'schedule-publish', 'cancel-publish'):
             raise ValueError('%s is not a valid action' % action)
         data = {'action': action}
         return self._call('blog-posts/%s/publish-action' % blog_post_id, data=json.dumps(data), method='POST', **options)
 
     def push_buffer_live(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_push_buffer_live'''
         return self._call('blog-posts/%s/push-buffer-live' % blog_post_id, method='POST', **options)
 
     def undelete_post(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_restore_deleted'''
         return self._call('blog-posts/%s/restore-deleted' % blog_post_id, method='POST', **options)
 
     def validate_buffer(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_validate_buffer'''
         return self._call('blog-posts/%s/validate-buffer' % blog_post_id, method='POST', **options)
 
     def get_post_versions(self, blog_post_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts_blog_post_id_versions'''
         return self._call('blog-posts/%s/versions' % blog_post_id, **options)
 
     def get_post_version(self, blog_post_id, version_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/get_blog_posts_blog_post_id_versions_version_id'''
         return self._call('blog-posts/%s/versions/%s' % (blog_post_id, version_id), **options)
 
     def restore_post_version(self, blog_post_id, version_id, **options):
+        '''https://developers.hubspot.com/docs/methods/blogv2/post_blog_posts_blog_post_id_versions_restore'''
         data = {'version_id': version_id}
         return self._call('blog-posts/%s/versions/restore' % blog_post_id, data=json.dumps(data), method='POST', **options)
 

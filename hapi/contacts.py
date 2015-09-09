40bcac6a4bd26d11fcd802e8a8dff1bfdd601183
Zac <zdelagrange@bitsighttech.com>
2015-09-09 18:43:28 -0400

    Tests for contacts & contact lists, added some methods to the API that helped me clean up after the testing on the public api

--- hapi/contacts.py
+++ hapi/contacts.py
@@ -19,15 +19,22 @@ class ContactsClient(BaseClient):
         return 'contacts/v%s/%s' % (self.options.get('version') or CONTACTS_API_VERSION, subpath)
 
     def create_or_update_a_contact(self, email, data=None, **options):
+        """ Creates or Updates a client with the supplied data. """
         data = data or {}
         return self._call('contact/createOrUpdate/email/{email}'.format(email=email),
                           data=data, method='POST', **options)
 
     def get_contact_by_email(self, email, **options):
+        """ Gets contact specified by email address. """
         return self._call('contact/email/{email}/profile'.format(email=email),
                           method='GET', **options)
 
     def update_a_contact(self, contact_id, data=None, **options):
+        """ Updates the contact by contact_id with the given data. """
         data = data or {}
         return self._call('contact/vid/{contact_id}/profile'.format(contact_id=contact_id),
                           data=data, method='POST', **options)
+
+    def delete_a_contact(self, contact_id, **options):
+        """ Deletes a contact by contact_id. """
+        return self._call('contact/vid/{contact_id}'.format(contact_id=contact_id), method='DELETE', **options)

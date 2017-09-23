9e21239bc416eb950db51e1b34290a84292146b9
Jonathan Goodwin <jgoodwin@hubspot.com>
2017-09-23 09:30:41 -0400

    Enable Blazar

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='hapipy',
-    version='2.10.5',
+    version='2.10.6',
     description="A python wrapper around HubSpot's APIs",
     long_description=open('README.md').read(),
     author='HubSpot Dev Team',
